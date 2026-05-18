f96d37f25c0bebd5b3ed80a78baddfb92dd16930
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-05-19 00:35:21 +0600

    Details page design started

---
+++ src/app/details/[id]/page.jsx
@@ -0,0 +1,12 @@
+import MyContainer from "@/components/shared/MyContainer";
+import React from "react";
+
+const DetailsPage = () => {
+  return (
+    <MyContainer className="pt-40">
+      <p>Details page</p>
+    </MyContainer>
+  );
+};
+
+export default DetailsPage;
